586e8eb2e02e3f2baf14b597cb036464b3edc14e
cs0041 <pamzalove20@hotmail.com>
2022-12-22 14:26:53 +0700

    printIn and Format Macros

--- rust-variables/src/main.rs
+++ rust-variables/src/main.rs
@@ -94,4 +94,26 @@ fn main() {
         first/second,
         first%second
     );
+
+    //printIn and Format Macros
+    let btc = "Bitcoin";
+    let eth = "Ethereum";
+    let sol = "Solana";
+ 
+    println!("Normal order: {} {} {}", btc, eth, sol);
+    println!("Flippening: {1} {0} {2} {2}", btc, eth, sol);
+    println!(
+        "Kwargs: {solana} {ethereum} {bitcoin}",
+        bitcoin=btc,
+        ethereum=eth,
+        solana=sol
+    );
+ 
+    let message = format!(        
+        "Kwargs: {solana} {ethereum} {bitcoin}",
+        bitcoin=btc,
+        ethereum=eth,
+        solana=sol
+    );
+    println!("Look I've made this special {message}!", message=message);
 }
