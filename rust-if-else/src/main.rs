5e22dff69f5b65bd8296f9de75d82d6db23b19ba
cs0041 <pamzalove20@hotmail.com>
2022-12-22 18:10:16 +0700

    Ranges in Match Statements

--- rust-if-else/src/main.rs
+++ rust-if-else/src/main.rs
@@ -68,4 +68,19 @@ fn main() {
  
     println!("{} {} {}", message, message2, message3);
 
+    // 7. handling ranges with conditionals
+    if name_len <= 5 {
+        println!("Short");
+    } else if name_len >= 6 && name_len < 11  {
+        println!("Medium");
+    } else {
+        println!("Long");
+    }
+ 
+    match name_len {
+       0..=5  => { println!("Short"); }
+       6..=10 => { println!("Medium"); }
+       _      => { println!("Long"); }
+    }
+    
 }
