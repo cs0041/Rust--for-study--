cb88f40fe5f9291471ee5dd217067b17b170102d
cs0041 <pamzalove20@hotmail.com>
2022-12-22 15:18:33 +0700

    Arrays

--- rust-variables/src/main.rs
+++ rust-variables/src/main.rs
@@ -164,5 +164,20 @@ fn main() {
         "{} ohlcv: {:#?}",
         token.0,
         token.2
-    );    
+    );
+
+    // Arrays 
+    // Rust arrays have a fixed length. 
+    // Each element in a Rust array has the same type.
+    // Essentially, arrays are like tuples, where the type of each element is fixed.
+    let cheat_code: [u32; 4] = [19, 65, 9, 17];
+    let zeros = [1.2; 10];
+ 
+    println!("Array: {:?}", cheat_code);
+    println!("First element of the array: {}", cheat_code[0]);
+ 
+    println!("Array(length: {}): {:?}", zeros.len() ,zeros);
+ 
+    let slice = &cheat_code[1..3];
+    println!("Slice of cheat_code: {:?} {}", slice, slice.len());
 }
