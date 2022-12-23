18174df6e312af0e86b415f52d8bba6298658310
cs0041 <pamzalove20@hotmail.com>
2022-12-23 23:34:24 +0700

    struct

---
+++ rust-struct/src/main.rs
@@ -0,0 +1,42 @@
+// Fields of a class are defined in a struct
+struct Candle {
+    ticker: String,
+    open: i64,
+    high: i64,
+    low: i64,
+    close: i64,
+    zeros: u32
+}
+// Methods of a class are defined using impl
+impl Candle {
+    // &self  - reference to this (see Javascript) or self (see python)
+    fn convert_value_to_f64(&self, value: i64) -> f64 {
+        value as f64 / (10_i64.pow(self.zeros)) as f64
+    }
+    fn get_delta(&self) -> f64 {
+        self.convert_value_to_f64( self.close - self.open )
+    }
+    fn get_info(&self) -> String {
+        format!(
+        "{} ({}, {}, {}, {})",
+        self.ticker,
+        self.convert_value_to_f64(self.open),
+        self.convert_value_to_f64(self.high),
+        self.convert_value_to_f64(self.low),
+        self.convert_value_to_f64(self.close)
+        )
+    }
+}
+fn main() {
+    let today: Candle = Candle {
+    ticker: "SOLUSDC".to_string(),
+    open: 9522,
+    high: 10883,
+    low: 9517,
+    close: 9981,
+    zeros: 2
+     };
+    println!("{}", today.get_delta());
+    println!("{}", today.get_info());
+   
+}
